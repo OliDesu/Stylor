5611d2db7174a2361f7224a569e49f5d4d35ed92
OliDesu <alielmufti25@gmail.com>
2024-07-06 21:20:51 +0200

    Initial Commit

---
+++ Stylor/StylorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylorApp.swift
+//  Stylor
+//
+//  Created by Ali El Mufti on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct StylorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
