ac4e00efb047cb25e3ccb7176d7ecfacadc62512
OliDesu <alielmufti25@gmail.com>
2024-07-15 23:37:39 +0200

    Adding best swiping

---
+++ Stylor/Core/Cards/Views/SwipeActionIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeActionIndicatorView.swift
+//  Stylor
+//
+//  Created by Ali El Mufti on 15/07/2024.
+//
+
+import SwiftUI
+
+struct SwipeActionIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeActionIndicatorView()
+}
