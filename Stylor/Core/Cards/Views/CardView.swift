7403b99a37bee70253837cdbbd59bd3142b8eaff
OliDesu <alielmufti25@gmail.com>
2024-07-16 20:08:50 +0200

    addidn yes and nope to the swipe

--- Stylor/Core/Cards/Views/CardView.swift
+++ Stylor/Core/Cards/Views/CardView.swift
@@ -13,14 +13,17 @@ struct CardView: View {
    // @State private var cardOpacity: Double = 100.0
     var body: some View {
         ZStack(alignment: .bottom){
-            Image(.messi)
-                .resizable()
+            ZStack(alignment:.top) {
+                Image(.messi)
+                    .resizable()
                 
+                SwipeActionIndicatorView(xOffset: $xOffset)
+            }
             UserInfoView()
                 .padding(.horizontal)
                 .foregroundStyle(.white)
         }
-        .frame(width: cardWidth,height: cardHeight)
+        .frame(width: SizeConstants.cardWidth,height: SizeConstants.cardHeight)
         .clipShape(RoundedRectangle( cornerRadius:10))
         .offset(x: xOffset)
       //  .opacity(cardOpacity)
@@ -44,25 +47,13 @@ private extension CardView{
     }
     func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value){
         let width = value.translation.width
-        if abs(width) <= abs(screenCutOff) {
+        if abs(width) <= abs(SizeConstants.screenCutOff) {
             xOffset = 0
             degrees = 0
         }
     }
 }
-extension CardView {
-    var screenCutOff: CGFloat {
-        (UIScreen.main.bounds.width / 2) *  0.8
 
-    }
-    var cardWidth: CGFloat {
-      UIScreen.main.bounds.width-20
-  }
-   
-   var cardHeight: CGFloat {
-      UIScreen.main.bounds.height / 1.45
-  }
-}
 #Preview {
     CardView()
 }
---
+++ Stylor/Utils/Constants/SizeConstants.swift
@@ -0,0 +1,22 @@
+//
+//  SizeConstants.swift
+//  Stylor
+//
+//  Created by Ali El Mufti on 16/07/2024.
+//
+
+import SwiftUI
+
+public struct SizeConstants {
+   static  var screenCutOff: CGFloat {
+      (UIScreen.main.bounds.width / 2) *  0.8
+
+    }
+   static var cardWidth: CGFloat {
+      UIScreen.main.bounds.width-20
+  }
+   
+   static var cardHeight: CGFloat {
+      UIScreen.main.bounds.height / 1.45
+  }
+}
