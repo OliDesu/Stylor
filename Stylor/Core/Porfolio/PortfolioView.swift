4da4646a30e78a6e63d510871e120b471580ea6a
OliDesu <alielmufti25@gmail.com>
2024-08-01 22:19:11 +0200

    Changing the display

--- Stylor/Core/Porfolio/PortfolioView.swift
+++ Stylor/Core/Porfolio/PortfolioView.swift
@@ -10,30 +10,30 @@ import SDWebImageSwiftUI
 
 struct PortfolioView: View {
     @State var currentUser : User = MockData.averageUser
+
     var body: some View {
         VStack {
             Text("Wallpapers")
                 .font(.system(.largeTitle, weight: .bold))
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .clipped()
                 .padding(.leading)
                 .padding(.bottom, 8)
+            
             LazyVGrid(columns: [GridItem(.flexible(), spacing: 4), GridItem(.flexible(), spacing: 4)], spacing: 4) {
-                ForEach(currentUser.userPortfolioImages, id:\.self) { img in // Replace with your data model here
+                ForEach(currentUser.userPortfolioImages, id: \.self) { img in
                     WebImage(url: img)
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fill)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                        .aspectRatio(1/1, contentMode: .fit)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .aspectRatio(1, contentMode: .fit)
                         .clipped()
-                        .mask { RoundedRectangle(cornerRadius: 30, style: .continuous) }
+                        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                        .accessibilityLabel("User uploaded image")
                 }
             }
             .padding(.horizontal)
         }
-        .frame(maxWidth: .infinity)
-        .clipped()
         .padding(.top, 98)
         .padding(.bottom, 150)
     }
