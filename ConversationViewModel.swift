458fd956a6b0281d1722f48ebe2cb4e66cc43fb4
OliDesu <alielmufti25@gmail.com>
2024-09-21 23:31:58 +0200

    Starting message view model

---
+++ ConversationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ConversationViewModel.swift
+//  Stylor
+//
+//  Created by Ali El Mufti on 21/09/2024.
+//
+
+import Foundation
--- Stylor/Presentation/TabBar/MainTabView.swift
+++ Stylor/Presentation/TabBar/MainTabView.swift
@@ -12,7 +12,7 @@ struct MainTabView: View {
         TabView {
             Text("Swiping View").tabItem { Image(systemName: "flame").tag(0) }
             ProfileSearchView().tabItem { Image(systemName: "magnifyingglass").tag(1) }
-            Text("Inbox View").tabItem { Image(systemName: "bubble").tag(2) }
+            ConversationView().tabItem { Image(systemName: "bubble").tag(2) }
             Text("Profile View").tabItem { Image(systemName: "person").tag(3) }
             PortfolioView().tabItem { Image(systemName: "person").tag(4) }
         }.tint(.primary)
